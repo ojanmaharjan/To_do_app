7d087f6a450af0de9b833c3808ee6d7d079df69f
ojan <ojanmaharjan03@gmail.com>
2025-06-12 21:49:26 +0545

    initilised

---
+++ FrontEnd/my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Login from './Login/Login'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Login/>
+    </>
+   
+  )
+}
+
+export default App
---
+++ FrontEnd/my-app/src/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './logIn.css'
+
+const Login = () => {
+  return (
+    <div className='logIn'>
+        <input type='name' placeholder='name' required/> <br></br>
+        <input type='password' placeholder='password' required/><br></br>
+        <input type='email' placeholder='email' required/><br></br>
+        <button type='submit'>Submit</button>
+    </div>
+  )
+}
+
+export default Login
